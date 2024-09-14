fabc593abc0a3bc8f7f78e0e464bc509d17171a3
Camelle <baloiscamelle@gmail.com>
2024-09-14 13:52:46 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,18 @@
+let favActorFirstName = "Coco";
+let favActorLastName = "Martin";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "Luna".toUpperCase(); 
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ", his best show is Silicon Valley.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
